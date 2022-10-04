623b8ab749a7c31c845b73d32572123ea3abde79
arka <arkadius.wellyam@bukalapak.com>
2022-10-04 15:15:18 +0700

    db seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'dogs/index'
-  get 'dogs/show'
-  get 'dogs/new'
-  get 'dogs/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :dogs
 end
